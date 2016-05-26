a9db5e6038f810103a4726b66faf94cc61b9a0fe
Matt Huddleston <mthuddleston@gmail.com>
2016-05-26 15:19:46 -0400

    Adding Admin view user screen.

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Invitation;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\UserType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin/view-users", name="view_users")
+     */
+    public function viewAllUsersAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('AppBundle:Admin:view_users.html.twig', array(
+            'users' => $users
+        ));
+    }
+
+    /**
+     * @Route("/admin/view-users/edit/{user_id}", name="admin_edit_user")
+     */
+    public function viewAdminEditUserAction(Request $request, $user_id)
+    {
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id' => $user_id));
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $event = new FormEvent($form, $request);
+            $userManager->updateUser($user);
+            $successMessage = "User information updated succesfully.";
+            $this->addFlash('notice', $successMessage);
+
+            return $this->redirectToRoute('view_users');
+        }
+        $em = $this->getDoctrine()->getManager();
+
+        return $this->render('@App/Admin/admin_edit_user.html.twig', array(
+            'form' => $form->createView(),
+            'user_id' => $user_id
+        ));
+    }
+
+
+    /**
+     * @Route("/admin/add-user", name="send_invitation")
+     */
+    public function sendInvitationAction(Request $request)
+    {
+        $invitation = new Invitation();
+        $officeRepository = $this->getDoctrine()->getRepository('AppBundle:Office');
+
+        $form = $this->createFormBuilder($invitation)
+            ->add('email', EmailType::class)
+            ->add('admin', CheckboxType::class, array(
+                'label' => 'Give user admin privileges?',
+                'required' => false,
+            ))
+            ->add('office', EntityType::class, array(
+                'class' => 'AppBundle:Office',
+                'label' => 'Office to assign to?',
+                'choice_label' => 'name',
+                'query_builder' => function (OfficeRepository $officeRepository) {
+                    return $officeRepository->createQueryBuilder('u')->orderBy('u.name', 'ASC');
+                }
+            ))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $data = $form->getData();
+
+            if($officeId = $data->getOffice()) {
+                $office = $officeRepository->find($officeId);
+                $invitation->setOffice($office);
+            }
+            else
+                $invitation->setOffice(null);
+
+            $email_service = $this->get('email_service');
+            $email_service->sendEmail(array(
+                    'subject' => 'Invitation to register for utus-orders.com',
+                    'from' => 'matt@245tech.com',
+                    'to' => $invitation->getEmail(),
+                    'body' => $this->renderView("AppBundle:Email:send_invitation_email.html.twig", array('code' => $invitation->getCode()))
+                )
+            );
+
+            $invitation->send();
+            $em->persist($invitation);
+            $em->flush();
+
+            $successMessage = "Invitation to " . $invitation->getEmail() . " sent succesfully.";
+            $this->addFlash('notice', $successMessage);
+
+            $invitation = new Invitation();
+            $form = $this->createFormBuilder($invitation)
+                ->add('email', EmailType::class)
+                ->add('admin', CheckboxType::class, array(
+                    'label' => 'Give user admin privileges?',
+                    'required' => false,
+                ))
+                ->add('office', EntityType::class, array(
+                    'class' => 'AppBundle:Office',
+                    'label' => 'Office to assign to?',
+                    'choice_label' => 'name'
+                ))
+                ->getForm();
+
+            return $this->render('@App/Security/send_invitation.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+
+        return $this->render('@App/Security/send_invitation.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/admin/add-user/sent-invitations", name="all_invitations")
+     */
+    public function showAllInvitationsAction(Request $request)
+    {
+        $conn = $this->get('database_connection');
+        $invitations = $conn->fetchAll('SELECT * FROM invitation');
+
+        return $this->render('@App/Security/invitations_sent.html.twig', array(
+            'invitations' => $invitations
+        ));
+    }
+
+
+}
