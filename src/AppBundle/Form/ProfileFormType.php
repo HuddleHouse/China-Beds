7d0ed0c1e277076383c8102fa57b386b7cbf38b9
Matt Huddleston <mthuddleston@gmail.com>
2016-05-27 14:29:55 -0400

    Adding new CRUD entities for Price Groups

---
+++ src/AppBundle/Form/ProfileFormType.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class ProfileFormType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('first_name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('last_name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('company_name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('address_1', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('address_2', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'), 'required' => false))
+            ->add('email', EmailType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('zip', NumberType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('city', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('state', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('groups', EntityType::class, array(
+                'class' => 'AppBundle:RoleGroup',
+                'label' => 'Role',
+                'choice_label' => 'name',
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'multiple' => true
+            ))
+            ->add('enabled', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Account Enabled',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_residential', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Residential Address?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_show_credit', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Hide Credit Form/Status?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_show_warranty', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Hide Warranty Form/Status?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_volume_discount', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Grant volume discount?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_charge_shipping', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Charge Shipping?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('sent_retail_kit', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Retailer Kit Sent?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_current', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Current?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+            ->add('is_online_intentions', ChoiceType::class, array(
+                'attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px'),
+                'label' => 'Online Intentions?',
+                'choices' => array(
+                    'Yes' => 1,
+                    'No' => 0,
+                ),
+            ))
+
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+}
