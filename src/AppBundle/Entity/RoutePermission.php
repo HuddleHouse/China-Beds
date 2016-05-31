03d6a4a97ccd727fbb66be7f9ea5f85c2d36d51c
Matt Huddleston <mthuddleston@gmail.com>
2016-05-31 17:24:21 -0400

    Adding error checking and form validation to all CRUD Entitities

---
+++ src/AppBundle/Entity/RoutePermission.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RoutePermission
+ *
+ * @ORM\Table(name="route_permissions")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RoutePermissionRepository")
+ */
+class RoutePermission
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role_name", type="string", length=255)
+     */
+    private $roleName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="route_name", type="string", length=255)
+     */
+    private $routeName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set roleName
+     *
+     * @param string $roleName
+     *
+     * @return RoutePermission
+     */
+    public function setRoleName($roleName)
+    {
+        $this->roleName = $roleName;
+
+        return $this;
+    }
+
+    /**
+     * Get roleName
+     *
+     * @return string
+     */
+    public function getRoleName()
+    {
+        return $this->roleName;
+    }
+
+    /**
+     * Set routeName
+     *
+     * @param string $routeName
+     *
+     * @return RoutePermission
+     */
+    public function setRouteName($routeName)
+    {
+        $this->routeName = $routeName;
+
+        return $this;
+    }
+
+    /**
+     * Get routeName
+     *
+     * @return string
+     */
+    public function getRouteName()
+    {
+        return $this->routeName;
+    }
+}
+
