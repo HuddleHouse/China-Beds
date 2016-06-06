1d4ca9921db11f4e8972601b3b8c7a0b22f28d8c
Matt Huddleston <mthuddleston@gmail.com>
2016-06-06 11:37:03 -0400

    Adding error checking.

---
+++ src/InventoryBundle/Controller/AttributeController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace InventoryBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use InventoryBundle\Entity\Attribute;
+use InventoryBundle\Form\AttributeType;
+
+/**
+ * Attribute controller.
+ *
+ * @Route("/attribute")
+ */
+class AttributeController extends Controller
+{
+    /**
+     * Lists all Attribute entities.
+     *
+     * @Route("/", name="attribute_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $attributes = $em->getRepository('InventoryBundle:Attribute')->findAll();
+
+        return $this->render('@Inventory/Attribute/index.html.twig', array(
+            'attributes' => $attributes,
+        ));
+    }
+
+    /**
+     * Creates a new Attribute entity.
+     *
+     * @Route("/new", name="attribute_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $attribute = new Attribute();
+        $form = $this->createForm('InventoryBundle\Form\AttributeType', $attribute);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $attribute->upload();
+            $em->persist($attribute);
+            $em->flush();
+
+            return $this->redirectToRoute('attribute_show', array('id' => $attribute->getId()));
+        }
+
+        return $this->render('@Inventory/Attribute/new.html.twig', array(
+            'attribute' => $attribute,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Attribute entity.
+     *
+     * @Route("/{id}", name="attribute_show")
+     * @Method("GET")
+     */
+    public function showAction(Attribute $attribute)
+    {
+        $deleteForm = $this->createDeleteForm($attribute);
+
+        return $this->render('@Inventory/Attribute/show.html.twig', array(
+            'attribute' => $attribute,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Attribute entity.
+     *
+     * @Route("/{id}/edit", name="attribute_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Attribute $attribute)
+    {
+        $deleteForm = $this->createDeleteForm($attribute);
+        $editForm = $this->createForm('InventoryBundle\Form\AttributeType', $attribute);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $attribute->upload();
+
+            $em->persist($attribute);
+            $em->flush();
+
+            return $this->redirectToRoute('attribute_edit', array('id' => $attribute->getId()));
+        }
+
+        return $this->render('@Inventory/Attribute/edit.html.twig', array(
+            'attribute' => $attribute,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Attribute entity.
+     *
+     * @Route("/{id}", name="attribute_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Attribute $attribute)
+    {
+        $form = $this->createDeleteForm($attribute);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($attribute);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('attribute_index');
+    }
+
+    /**
+     * Creates a form to delete a Attribute entity.
+     *
+     * @param Attribute $attribute The Attribute entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Attribute $attribute)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('attribute_delete', array('id' => $attribute->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/InventoryBundle/Form/AttributeType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace InventoryBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class AttributeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom: 10px')))
+            ->add('file', FileType::class, array(
+                'attr' => array(
+                    'style' => 'margin-bottom: 29px'),
+                'label' => 'Picture',
+                'required' => false,
+            ));
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'InventoryBundle\Entity\Attribute'
+        ));
+    }
+}
