25a425805009ca27bf1001fb9746eeef263c230a
Matt Huddleston <mthuddleston@gmail.com>
2016-08-29 12:56:48 -0400

    sdfsafasdf

--- src/InventoryBundle/Controller/DefaultController.php
+++ src/InventoryBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('InventoryBundle:Default:index.html.twig');
+        return $this->redirectToRoute('fos_user_security_login');
     }
 }
