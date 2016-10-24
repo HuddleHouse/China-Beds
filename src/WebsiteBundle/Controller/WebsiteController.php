620d72f13248040fb8532d6699415c908122875e
Jeremi Bergman <jeremib@gmail.com>
2016-10-24 16:54:58 -0400

    Add support for website bundle and separate app listener

--- src/WebsiteBundle/Controller/WebsiteController.php
+++ src/WebsiteBundle/Controller/WebsiteController.php
@@ -5,101 +5,91 @@ namespace WebsiteBundle\Controller;
 use InventoryBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class WebsiteController extends Controller
+class WebsiteController extends BaseController
 {
-    public function indexAction($site)
+    public function indexAction()
     {
         return $this->render('WebsiteBundle:Website:home.html.twig', array(
-            'site' => $site
+            'site' => strtolower($this->getChannel()->getName())
         ));
     }
 
-    public function faqIndexAction($site)
+    public function faqIndexAction()
     {
         return $this->render('WebsiteBundle:Website:faq.html.twig', array(
-            'site' => $site
+            'site' => strtolower($this->getChannel()->getName())
         ));
     }
 
-    public function productFeaturesIndexAction($site)
+    public function productFeaturesIndexAction()
     {
         return $this->render('WebsiteBundle:Website:product-features.html.twig', array(
-            'site' => $site
+            'site' => strtolower($this->getChannel()->getName())
         ));
     }
 
-    public function retailerIndexAction($site)
+    public function retailerIndexAction()
     {
         return $this->render('WebsiteBundle:Website:retailer.html.twig', array(
-            'site' => $site
+            'site' => strtolower($this->getChannel()->getName())
         ));
     }
 
-    public function warrantyIndexAction($site)
+    public function warrantyIndexAction()
     {
         return $this->render('WebsiteBundle:Website:warranty.html.twig', array(
-            'site' => $site
+            'site' => strtolower($this->getChannel()->getName())
         ));
     }
 
-    public function contactIndexAction($site)
+    public function contactIndexAction()
     {
         return $this->render('WebsiteBundle:Website:contact.html.twig', array(
-            'site' => $site
+            'site' => strtolower($this->getChannel()->getName())
         ));
     }
 
-    public function  mattressIndexAction($site) {
+    public function  mattressIndexAction() {
         $em = $this->getDoctrine()->getEntityManager();
 
-        if($site == 'mlily')
-            $channel = $em->getRepository('InventoryBundle:Channel')->findOneBy(array('name' => 'MLILY'));
-        else
-            $channel = $em->getRepository('InventoryBundle:Channel')->findOneBy(array('name' => 'BedBoss'));
+        $channel = $this->getChannel();
 
         $mattresses = $em->getRepository('InventoryBundle:Product')->getAllMattressesForChannelArray($channel);
 
         return $this->render('WebsiteBundle:Website:mattresses.html.twig', array(
-            'site' => $site,
+            'site' => strtolower($this->getChannel()->getName()),
             'mattresses' => $mattresses
         ));
     }
 
-    public function  singleMattressIndexAction($site, Product $product) {
+    public function  singleMattressIndexAction(Product $product) {
         return $this->render('WebsiteBundle:Website:single-mattresses.html.twig', array(
-            'site' => $site,
+            'site' => strtolower($this->getChannel()->getName()),
             'product' => $product
         ));
     }
 
-    public function  pillowsIndexAction($site) {
+    public function  pillowsIndexAction() {
         $em = $this->getDoctrine()->getEntityManager();
 
-        if($site == 'mlily')
-            $channel = $em->getRepository('InventoryBundle:Channel')->findOneBy(array('name' => 'MLILY'));
-        else
-            $channel = $em->getRepository('InventoryBundle:Channel')->findOneBy(array('name' => 'BedBoss'));
-
+        $channel = $this->getChannel();
         $pillows = $em->getRepository('InventoryBundle:Product')->getAllPillowsForChannelArray($channel);
 
         return $this->render('WebsiteBundle:Website:pillows.html.twig', array(
-            'site' => $site,
+            'site' => strtolower($this->getChannel()->getName()),
             'pillows' => $pillows
         ));
     }
 
-    public function  adjustablesIndexAction($site) {
+    public function  adjustablesIndexAction() {
         $em = $this->getDoctrine()->getEntityManager();
 
-        if($site == 'mlily')
-            $channel = $em->getRepository('InventoryBundle:Channel')->findOneBy(array('name' => 'MLILY'));
-        else
-            $channel = $em->getRepository('InventoryBundle:Channel')->findOneBy(array('name' => 'BedBoss'));
+        $channel = $this->getChannel();
 
         $adjustables = $em->getRepository('InventoryBundle:Product')->getAllAdjustablesForChannelArray($channel);
 
         return $this->render('WebsiteBundle:Website:adjustables.html.twig', array(
-            'site' => $site,
+            'site' => strtolower($this->getChannel()->getName()),
             'adjustables' => $adjustables
         ));
     }
