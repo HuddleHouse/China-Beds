a0431b4e1e57cf51c61d72c5765d3ca410559dec
Jeremi Bergman <jeremib@gmail.com>
2016-10-26 11:43:50 -0400

    Enable caching on production, change channel config around

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+export SYMFONY_ENV=prod
 git pull
+sudo rm -Rf var/cache/*
 composer install --no-dev --optimize-autoloader
 sudo service apache2 restart
-sudo rm -Rf var/cache/*
 sudo chmod -Rf 777 var/cache
 sudo chmod -Rf 777 var/logs
-php app/console cache:clear --env=prod --no-debug
-php app/console assetic:dump --env=prod --no-debug
-php app/console doctrine:schema:update --force --env=prod --no-debug
+php bin/console cache:clear --env=prod --no-debug
+php bin/console assetic:dump --env=prod --no-debug
+php bin/console doctrine:schema:update --force --env=prod --no-debug
 sudo chmod -Rf 777 var/cache
 sudo chmod -Rf 777 var/logs
