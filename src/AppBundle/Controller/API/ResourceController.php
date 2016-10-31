5dd300cd459ef8c0b33f0e35f2e07fa2a022d56c
richard <rajeffords@gmail.com>
2016-10-31 04:18:20 -0400

    way faster resource upload

--- src/AppBundle/Controller/API/ResourceController.php
+++ src/AppBundle/Controller/API/ResourceController.php
@@ -39,7 +39,7 @@ class ResourceController extends Controller
             $rtn = array(
                 '<a href="/' . $resource->getWebPath() . '" target="_blank">' . $resource->getName() . '</a>',
                 $resource->getDateCreated()->format('m/d/y h:i:s A'),
-                '<a download class="btn btn-raised btn-danger btn-xs" href="/' . $resource->getWebPath() . '">Download</a><button class="btn btn-raised btn-xs" onclick="deleteResource('. $resource->getId() .', this)">Delete</button>'
+                '<a download class="btn btn-raised btn-xs" href="/' . $resource->getWebPath() . '">Download</a><button class="btn btn-raised btn-danger btn-xs" onclick="deleteResource('. $resource->getId() .', this)">Delete</button>'
             );
 
             return new JsonResponse(array(true, $rtn));
