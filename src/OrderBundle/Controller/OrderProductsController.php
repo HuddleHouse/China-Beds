c19f20d7f8d3ade92e294de158e3789e4664b785
Colin Rickels <colin@245tech.com>
2016-11-08 18:38:48 -0500

    fix pop item image display

--- src/OrderBundle/Controller/OrderProductsController.php
+++ src/OrderBundle/Controller/OrderProductsController.php
@@ -82,7 +82,50 @@ class OrderProductsController extends Controller
         else
             $this->redirectToRoute('404');
 
-        $pop = $em->getRepository('InventoryBundle:PopItem')->getAllPopItemsArrayForCart($channel);
+
+        //$pop = $em->getRepository('InventoryBundle:PopItem')->getAllPopItemsArrayForCart($channel,);
+
+
+        //
+
+        $pop = $em->getRepository('InventoryBundle:PopItem')->findBy(['channel' => $channel, 'is_hide_on_order' => 0, 'active' => 1]);
+        $data = array();
+
+        if(isset($pop)){
+            foreach($pop as $popitem) {
+                $connection = $em->getConnection();
+                $statement = $connection->prepare("
+select coalesce(sum(i.quantity), 0) as quantity
+		from warehouse_pop_inventory i
+		where i.pop_item_id = :pop_item_id");
+                $statement->bindValue('pop_item_id', $popitem->getId());
+                $statement->execute();
+                $quantity_data = $statement->fetch();
+                $quantity = (int)$quantity_data['quantity'];
+
+                $data[] = array(
+                    'id' => $popitem->getId(),
+                    'cost' => $popitem->getPricePer(),
+                    'name' => $popitem->getName(),
+                    'description' => $popitem->getDescription(),
+                    'picture' => '/uploads/documents/' . $popitem->getPath(),
+                    'type' => 'pop',
+                    'inventory' => $quantity //get actually inventory one day
+                );
+            }
+        }else{
+            $data[] = array(
+                'id' => '',
+                'cost' => '',
+                'name' => '',
+                'description' => '',
+                'picture' => '',
+                'type' => '',
+                'inventory' => ''
+            );
+        }
+
+        //
 
         $user_warehouses[] = array('id' => $user->getWarehouse1()->getId(), 'name' => $user->getWarehouse1()->getName());
         $user_warehouses[] = array('id' => $user->getWarehouse2()->getId(), 'name' => $user->getWarehouse2()->getName());
@@ -106,7 +149,7 @@ class OrderProductsController extends Controller
             'user' => $user,
             'user_warehouses' => $user_warehouses,
             'user_retailers' => $user_retailers,
-            'pop' => $pop,
+            'pop' => $data,
             'is_edit' => 0
       ));
     }
