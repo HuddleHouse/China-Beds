b1e03c963b6de57d628e2cd8403356c6583fdba8
Colin Rickels <colin@245tech.com>
2016-12-28 10:18:40 -0500

    completed order deltion

--- src/OrderBundle/Controller/OrderProductsController.php
+++ src/OrderBundle/Controller/OrderProductsController.php
@@ -493,11 +493,23 @@ select coalesce(sum(i.quantity), 0) as quantity
     /**
      * @Route("/delete/{id_channel}/{id_order}", name="order_products_delete")
      */
-    public function deleteOrderAction(Channel $channel, Orders $order)
+    public function deleteOrderAction($id_channel, $id_order)
     {
-        $that = $channel;
-        $that2 = $order;
-        $that3 = "something";
+        try{
+            $em = $this->getDoctrine()->getManager();
+
+            $channel = $em->getRepository("InventoryBundle:Channel")->find($id_channel);
+            $order = $em->getRepository("OrderBundle:Orders")->find($id_order);
+
+            $em->remove($order);
+
+            $em->flush();
+            $this->addFlash('notice', 'order deleted');
+            return $this->redirectToRoute('my_orders_index');
+        }catch(\Exception $e){
+            $this->addFlash('notice', 'Problem: ' . $e );
+            return $this->redirectToRoute('my_orders_index');
+        }
 
     }
 
